92522607f80b9f8c513c7f04eb34f6a28720c521
Felix Angelov <felangelov@gmail.com>
2022-05-03 23:57:51 -0500

    test(dart_frog): 100% test coverage (#1)

---
+++ packages/dart_frog/lib/src/path_to_route.dart
@@ -0,0 +1,20 @@
+import 'package:path/path.dart' as p;
+
+/// Convert a file path to an API route.
+///
+/// ```
+/// "../routes/index.dart" -> "/"
+/// "../routes/hello.dart" -> "/hello"
+/// "../routes/hello/world.dart" -> "/hello/world"
+/// "../routes/hello/<name>.dart" -> "/hello/<name>"
+/// ```
+String pathToRoute(String path) {
+  final relativePath = p.relative(path, from: '../routes');
+  final route = '/${relativePath.split('.dart').first.replaceAll('index', '')}';
+
+  if (route.length > 1 && route.endsWith('/')) {
+    return route.substring(0, route.length - 1);
+  }
+
+  return route;
+}
---
+++ packages/dart_frog/test/src/path_to_route_test.dart
@@ -0,0 +1,20 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('pathToRoute', () {
+    final expectedPathToRouteMappings = <String, String>{
+      '../routes/index.dart': '/',
+      '../routes/hello.dart': '/hello',
+      '../routes/hello/world.dart': '/hello/world',
+      '../routes/hello/<name>.dart': '/hello/<name>',
+      '../routes/api/v1/index.dart': '/api/v1',
+    };
+
+    for (final entry in expectedPathToRouteMappings.entries) {
+      test('maps ${entry.key} -> ${entry.value}', () {
+        expect(pathToRoute(entry.key), equals(entry.value));
+      });
+    }
+  });
+}
