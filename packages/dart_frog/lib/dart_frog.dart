a3d2c0e2f4a2a6d260983f18a41f1ef47870ed3d
Felix Angelov <felangelov@gmail.com>
2022-05-08 23:54:28 -0500

    move hot reload

--- packages/dart_frog/lib/dart_frog.dart
+++ packages/dart_frog/lib/dart_frog.dart
@@ -4,7 +4,6 @@ library dart_frog;
 export 'package:shelf/shelf.dart'
     show Handler, Middleware, Pipeline, Request, Response, logRequests;
 export 'package:shelf/shelf_io.dart' show serve;
-export 'package:shelf_hotreload/shelf_hotreload.dart' show withHotreload;
 export 'package:shelf_router/shelf_router.dart' show Router, RouterParams;
 
 export 'src/middleware.dart' show HandlerUse;
--- packages/dart_frog_gen/lib/dart_frog_gen.dart
+++ packages/dart_frog_gen/lib/dart_frog_gen.dart
@@ -2,5 +2,6 @@
 library dart_frog_gen;
 
 export 'package:dart_frog/dart_frog.dart' show Pipeline, Router, serve;
+export 'package:shelf_hotreload/shelf_hotreload.dart' show withHotreload;
 
 export 'src/path_to_route.dart';
