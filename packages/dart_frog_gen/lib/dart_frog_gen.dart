892a3ab3b4b0e069aa223b57e289492c5db892ac
Felix Angelov <felangelov@gmail.com>
2022-05-08 23:52:32 -0500

    refactor: migrate path_to_route to dart_frog_gen

--- packages/dart_frog_gen/lib/dart_frog_gen.dart
+++ packages/dart_frog_gen/lib/dart_frog_gen.dart
@@ -1,3 +1,6 @@
+/// Code generation tooling for package:dart_frog
 library dart_frog_gen;
 
-export 'src/dart_frog_gen.dart';
+export 'package:dart_frog/dart_frog.dart' show Pipeline, Router, serve;
+
+export 'src/path_to_route.dart';
