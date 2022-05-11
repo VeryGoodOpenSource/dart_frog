fca3be81785a5525e0fd05086c6851261872f73c
Felix Angelov <felangelov@gmail.com>
2022-05-11 00:13:54 -0500

    tweak

--- packages/dart_frog_gen/lib/dart_frog_gen.dart
+++ packages/dart_frog_gen/lib/dart_frog_gen.dart
@@ -1,5 +1,5 @@
 /// Code generation tooling for package:dart_frog
 library dart_frog_gen;
 
-export 'src/build_route_graph.dart';
+export 'src/build_route_configuration.dart';
 export 'src/path_to_route.dart';
