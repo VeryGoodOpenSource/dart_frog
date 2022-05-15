0a942b9b1021c58b765b4afab92b029e6a23747d
Felix Angelov <felangelov@gmail.com>
2022-05-14 21:57:35 -0500

    feat(dart_frog): add JsonResponse

--- packages/dart_frog/lib/dart_frog.dart
+++ packages/dart_frog/lib/dart_frog.dart
@@ -7,5 +7,6 @@ export 'package:shelf/shelf_io.dart' show serve;
 export 'package:shelf_hotreload/shelf_hotreload.dart' show withHotreload;
 export 'package:shelf_router/shelf_router.dart' show Router, RouterParams;
 
+export 'src/json_response.dart' show JsonResponse;
 export 'src/middleware.dart' show HandlerUse;
 export 'src/provide.dart' show HandlerProvide, read;
