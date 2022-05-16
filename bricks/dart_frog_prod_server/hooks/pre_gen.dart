8ef97bf5de9ada10a9dd64c1707ad7a702d884ea
Felix Angelov <felangelov@gmail.com>
2022-05-15 22:49:24 -0500

    fix: prod server docker package support

--- bricks/dart_frog_prod_server/hooks/pre_gen.dart
+++ bricks/dart_frog_prod_server/hooks/pre_gen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dart_frog_gen/dart_frog_gen.dart';
 import 'package:mason/mason.dart';
 import 'package:path/path.dart' as path;
+import 'package:pubspec_parse/pubspec_parse.dart';
 
 Future<void> run(HookContext context) async {
   final projectDirectory = Directory.current;
@@ -10,6 +11,20 @@ Future<void> run(HookContext context) async {
   final buildDirectory = Directory(buildDirectoryPath);
   final dartFrogDirectoryPath = path.join(projectDirectory.path, '.dart_frog');
   final dartFrogDirectory = Directory(dartFrogDirectoryPath);
+
+  final pubspec = Pubspec.parse(
+    await File(path.join(projectDirectory.path, 'pubspec.yaml')).readAsString(),
+  );
+
+  final dependencies = pubspec.dependencies;
+  final devDependencies = pubspec.devDependencies;
+  final pathDependencies = [
+    ...dependencies.entries,
+    ...devDependencies.entries,
+  ]
+      .where((entry) => entry.value is PathDependency)
+      .map((entry) => (entry.value as PathDependency).path)
+      .toList();
 
   final bundlingDone = context.logger.progress('Bundling sources');
   if (await buildDirectory.exists()) {
@@ -55,5 +70,6 @@ Future<void> run(HookContext context) async {
     'globalMiddleware': configuration.globalMiddleware != null
         ? configuration.globalMiddleware!.toJson()
         : false,
+    'pathDependencies': pathDependencies,
   };
 }
