c66708530581a90f96e9b339df9cda9ead04e6fd
Felix Angelov <felangelov@gmail.com>
2022-05-22 17:06:32 -0500

    refactor(bricks): use upgraded dart_frog_gen and improve brick code quality

--- bricks/dart_frog_dev_server/__brick__/server.dart
+++ bricks/dart_frog_dev_server/__brick__/server.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_single_quotes, implicit_dynamic_list_literal, prefer_const_constructors, lines_longer_than_80_chars, avoid_dynamic_calls
+// ignore_for_file: prefer_single_quotes, implicit_dynamic_list_literal, prefer_const_constructors, lines_longer_than_80_chars, avoid_dynamic_calls, library_prefixes, directives_ordering
 
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
-import 'package:path/path.dart' as path;
 
 {{#routes}}import '{{{path}}}' as {{#snakeCase}}{{{name}}}{{/snakeCase}};
 {{/routes}}
@@ -14,19 +13,19 @@ void main() => withHotreload(createServer);
 Future<HttpServer> createServer() async {
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final handler = buildHandler();
+  final handler = buildRootHandler();
   return serve(handler, ip, port);
 }
 
-Handler buildHandler() {
-  var pipeline = const Pipeline();{{#globalMiddleware}}pipeline = pipeline.addMiddleware({{#snakeCase}}{{{name}}}{{/snakeCase}}.middleware);{{/globalMiddleware}}
+Handler buildRootHandler() {
+  final pipeline = const Pipeline(){{#globalMiddleware}}.addMiddleware({{#snakeCase}}{{{name}}}{{/snakeCase}}.middleware){{/globalMiddleware}};
   final router = Router(){{#directories}}
-    ..mount('{{{route}}}', (r) => build{{#pascalCase}}{{{name}}}{{/pascalCase}}Router()(r)){{/directories}};
+    ..mount('{{{route}}}', (r) => build{{#pascalCase}}{{{name}}}{{/pascalCase}}Handler()(r)){{/directories}};
   return pipeline.addHandler(router);
 }
 {{#directories}}
-Handler build{{#pascalCase}}{{{name}}}{{/pascalCase}}Router() {
-  var pipeline = const Pipeline();{{#middleware}}pipeline = pipeline.addMiddleware({{#snakeCase}}{{{name}}}{{/snakeCase}}.middleware);{{/middleware}}
+Handler build{{#pascalCase}}{{{name}}}{{/pascalCase}}Handler() {
+  {{#middleware}}final pipeline = const Pipeline(){{#middleware}}.addMiddleware({{#snakeCase}}{{{name}}}{{/snakeCase}}.middleware);{{/middleware}}{{/middleware}}{{^middleware}}const pipeline = Pipeline();{{/middleware}}
   final router = Router()
     {{#files}}..all('{{{route}}}', {{#snakeCase}}{{{name}}}{{/snakeCase}}.onRequest){{/files}};
   return pipeline.addHandler(router);
