e1b06e87d07315ccbe1fd0eaa4c8c4730e2bcbfe
Felix Angelov <felangelov@gmail.com>
2022-05-23 15:36:46 -0500

    tweak

--- bricks/create_dart_frog/__brick__/tests/routes/index_test.dart
+++ bricks/create_dart_frog/__brick__/tests/routes/index_test.dart
@@ -10,7 +10,7 @@ class _MockRequestContext extends Mock implements RequestContext {}
 
 void main() {
   group('GET /', () {
-    test('responds with a 200 and "Welcome to Dart Frog!".', () {      
+    test('responds with a 200 and "Welcome to Dart Frog!".', () {
       final context = _MockRequestContext();
       final response = route.onRequest(context);
       expect(response.statusCode, equals(HttpStatus.ok));
