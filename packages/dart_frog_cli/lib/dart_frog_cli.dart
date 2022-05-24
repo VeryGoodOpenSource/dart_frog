3a00c4ff664942b28b0e20528a3513b5b1408217
Felix Angelov <felangelov@gmail.com>
2022-05-24 13:45:23 -0500

    tweak

--- packages/dart_frog_cli/lib/dart_frog_cli.dart
+++ packages/dart_frog_cli/lib/dart_frog_cli.dart
@@ -1,1 +1,10 @@
+/// The official command line interface for Dart Frog
+///
+/// ```sh
+/// # activate dart_frog_cli
+/// dart pub global activate dart_frog_cli
+///
+/// # see usage
+/// dart_frog --help
+/// ```
 library dart_frog_cli;
