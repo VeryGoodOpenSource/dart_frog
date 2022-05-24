914a69408fa0fa8ff65a2e12b139ed9b8021d07b
Felix Angelov <felangelov@gmail.com>
2022-05-24 13:24:11 -0500

    chore(dart_frog): add example

---
+++ packages/dart_frog/example/main.dart
@@ -0,0 +1,6 @@
+import 'package:dart_frog/dart_frog.dart';
+
+// Learn more at https://github.com/VeryGoodOpenSource/dart_frog
+Response onRequest(RequestContext context) {
+  return Response(body: 'Welcome to Dart Frog!');
+}
