6384fd723ff73662e1642ba84e27a237e4245472
Felix Angelov <felangelov@gmail.com>
2022-05-23 23:15:24 -0500

    format

--- bricks/create_dart_frog/__brick__/routes/index.dart
+++ bricks/create_dart_frog/__brick__/routes/index.dart
@@ -1,3 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) => Response.ok('Welcome to Dart Frog!');
+Response onRequest(RequestContext context) {
+  return Response('Welcome to Dart Frog!');
+}
