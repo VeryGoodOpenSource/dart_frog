4d4b17c3b3a49d16f94db9ed9ac659bfebdb746a
Felix Angelov <felangelov@gmail.com>
2022-05-24 13:40:16 -0500

    use stdout

--- packages/dart_frog_gen/example/main.dart
+++ packages/dart_frog_gen/example/main.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: avoid_print
-
 import 'dart:io';
 
 import 'package:dart_frog_gen/dart_frog_gen.dart';
 
 void main() {
   final routeConfiguration = buildRouteConfiguration(Directory.current);
-  print(routeConfiguration);
+  stdout.writeln(routeConfiguration.toString());
 }
