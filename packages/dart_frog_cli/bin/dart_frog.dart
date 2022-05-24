9b4838fd3de9ae9278a98aca94986cf1f0c67aeb
Felix Angelov <felangelov@gmail.com>
2022-05-24 12:05:09 -0500

    chore(dart_frog_cli): remove package:universal_io

--- packages/dart_frog_cli/bin/dart_frog.dart
+++ packages/dart_frog_cli/bin/dart_frog.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
+
 import 'package:dart_frog_cli/src/command_runner.dart';
-import 'package:universal_io/io.dart';
 
 Future<void> main(List<String> args) async {
   final exitCode = await DartFrogCommandRunner().run(args);
