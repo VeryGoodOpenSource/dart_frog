d437260c59cc2709d5a5c8bf970bb132400f0d7b
Felix Angelov <felangelov@gmail.com>
2022-05-25 18:42:42 -0500

    docs: update example and README dynamic routes

--- example/test/routes/echo/[message]_test.dart
+++ example/test/routes/echo/[message]_test.dart
@@ -4,7 +4,7 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../../routes/echo/<message>.dart' as route;
+import '../../../routes/echo/[message].dart' as route;
 
 class _MockRequestContext extends Mock implements RequestContext {}
 
