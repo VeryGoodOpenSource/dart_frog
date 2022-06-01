946ba40aeee75c7a6f43580c6a708aaf7d6153b5
Felix Angelov <felangelov@gmail.com>
2022-05-31 23:49:07 -0500

    fix(dart_frog_cli): kill dev server child process on windows

--- packages/dart_frog_cli/test/src/commands/dev/dev_test.dart
+++ packages/dart_frog_cli/test/src/commands/dev/dev_test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:dart_frog_cli/src/commands/commands.dart';
@@ -16,6 +17,10 @@ class _MockGeneratorHooks extends Mock implements GeneratorHooks {}
 
 class _MockProcess extends Mock implements Process {}
 
+class _MockProcessResult extends Mock implements ProcessResult {}
+
+class _MockProcessSignal extends Mock implements ProcessSignal {}
+
 class _FakeDirectoryGeneratorTarget extends Fake
     implements DirectoryGeneratorTarget {}
 
@@ -26,21 +31,31 @@ void main() {
     });
 
     late DirectoryWatcher directoryWatcher;
+    late bool isWindows;
     late Logger logger;
     late Process process;
+    late ProcessResult processResult;
+    late ProcessSignal sigint;
     late MasonGenerator generator;
     late DevCommand command;
 
     setUp(() {
       directoryWatcher = _MockDirectoryWatcher();
+      isWindows = false;
       logger = _MockLogger();
       when(() => logger.progress(any())).thenReturn(([_]) {});
       process = _MockProcess();
+      processResult = _MockProcessResult();
+      sigint = _MockProcessSignal();
       generator = _MockMasonGenerator();
       command = DevCommand(
         logger: logger,
         directoryWatcher: (_) => directoryWatcher,
         generator: (_) async => generator,
+        isWindows: isWindows,
+        runProcess: (String executable, List<String> arguments) async {
+          return processResult;
+        },
         startProcess: (
           String executable,
           List<String> arguments, {
@@ -48,6 +63,7 @@ void main() {
         }) async {
           return process;
         },
+        sigint: sigint,
       );
     });
 
@@ -82,6 +98,69 @@ void main() {
       );
       final exitCode = await command.run();
       expect(exitCode, equals(ExitCode.success.code));
+    });
+
+    test('kills all child processes when sigint received on windows', () async {
+      const processId = 42;
+      final generatorHooks = _MockGeneratorHooks();
+      final processRunCalls = <List<String>>[];
+      int? exitCode;
+      when(
+        () => generatorHooks.preGen(
+          vars: any(named: 'vars'),
+          workingDirectory: any(named: 'workingDirectory'),
+          onVarsChanged: any(named: 'onVarsChanged'),
+        ),
+      ).thenAnswer((invocation) async {
+        (invocation.namedArguments[const Symbol('onVarsChanged')] as Function(
+          Map<String, dynamic> vars,
+        ))
+            .call(<String, dynamic>{});
+      });
+      when(
+        () => generator.generate(
+          any(),
+          vars: any(named: 'vars'),
+          fileConflictResolution: FileConflictResolution.overwrite,
+        ),
+      ).thenAnswer((_) async => []);
+      when(() => generator.hooks).thenReturn(generatorHooks);
+      when(() => process.stdout).thenAnswer((_) => const Stream.empty());
+      when(() => process.stderr).thenAnswer((_) => const Stream.empty());
+      when(() => process.pid).thenReturn(processId);
+      when(() => processResult.exitCode).thenReturn(ExitCode.success.code);
+      when(
+        () => directoryWatcher.events,
+      ).thenAnswer((_) => StreamController<WatchEvent>().stream);
+      when(() => sigint.watch()).thenAnswer((_) => Stream.value(sigint));
+      command = DevCommand(
+        logger: logger,
+        directoryWatcher: (_) => directoryWatcher,
+        generator: (_) async => generator,
+        exit: (code) => exitCode = code,
+        isWindows: true,
+        runProcess: (String executable, List<String> arguments) async {
+          processRunCalls.add([executable, ...arguments]);
+          return processResult;
+        },
+        startProcess: (
+          String executable,
+          List<String> arguments, {
+          bool runInShell = false,
+        }) async {
+          return process;
+        },
+        sigint: sigint,
+      );
+      command.run().ignore();
+      await untilCalled(() => process.pid);
+      expect(exitCode, equals(ExitCode.success.code));
+      expect(
+        processRunCalls,
+        equals([
+          ['taskkill', '/F', '/T', '/PID', '$processId']
+        ]),
+      );
     });
   });
 }
