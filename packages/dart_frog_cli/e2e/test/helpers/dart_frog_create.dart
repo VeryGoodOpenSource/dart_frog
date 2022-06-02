dfe5d49aec05d60089d409be6b48e6777885c51c
Felix Angelov <felangelov@gmail.com>
2022-06-02 16:30:43 -0500

    test(dart_frog_cli): add e2e tests (#71)

---
+++ packages/dart_frog_cli/e2e/test/helpers/dart_frog_create.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+Future<void> dartFrogCreate({
+  required String projectName,
+  required Directory directory,
+}) async {
+  final result = await Process.run(
+    'dart_frog',
+    ['create', projectName],
+    workingDirectory: directory.path,
+  );
+  if (result.exitCode != 0) {
+    throw Exception('dart_frog create exited with code $exitCode');
+  }
+}
