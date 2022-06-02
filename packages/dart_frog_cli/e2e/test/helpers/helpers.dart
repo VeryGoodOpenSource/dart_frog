953afb312277cdd5a17dad2b54bc2cd69d04f583
Felix Angelov <felangelov@gmail.com>
2022-06-02 17:16:29 -0500

    tweak

--- packages/dart_frog_cli/e2e/test/helpers/helpers.dart
+++ packages/dart_frog_cli/e2e/test/helpers/helpers.dart
@@ -2,6 +2,6 @@ export 'dart_analyze.dart';
 export 'dart_format.dart';
 export 'dart_frog_create.dart';
 export 'dart_frog_dev.dart';
-export 'dart_test.dart';
+export 'dart_tst.dart';
 export 'kill_dart_frog_server.dart';
 export 'test_server.dart';
