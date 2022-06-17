9c15c6e4a3837ad326b0b69d7f8f4ba743c73349
Felix Angelov <felangelov@gmail.com>
2022-06-17 15:01:47 -0500

    feat(dart_frog): toShelfMiddleware/toShelfHandler adapters (#97)

--- packages/dart_frog/lib/src/shelf_adapters.dart
+++ packages/dart_frog/lib/src/shelf_adapters.dart
@@ -15,10 +15,32 @@ Middleware fromShelfMiddleware(shelf.Middleware middleware) {
   };
 }
 
+/// Convert from [Middleware] into [shelf.Middleware].
+shelf.Middleware toShelfMiddleware(Middleware middleware) {
+  return (innerHandler) {
+    return (request) async {
+      final response = await middleware((context) async {
+        final response = await innerHandler(context.request._request);
+        return Response._(response);
+      })(RequestContext._(request));
+      return response._response;
+    };
+  };
+}
+
 /// Convert from a [shelf.Handler] into a [Handler].
 Handler fromShelfHandler(shelf.Handler handler) {
   return (context) async {
     final response = await handler(context.request._request);
     return Response._(response);
+  };
+}
+
+/// Convert from a [Handler] into a [shelf.Handler].
+shelf.Handler toShelfHandler(Handler handler) {
+  return (request) async {
+    final context = RequestContext._(request);
+    final response = await handler.call(context);
+    return response._response;
   };
 }
