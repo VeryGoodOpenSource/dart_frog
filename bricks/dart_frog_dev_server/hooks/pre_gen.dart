b44dd86e736973079a701311d8f41679eb5c4513
Felix Angelov <felangelov@gmail.com>
2022-07-20 16:57:11 -0500

    feat(dart_frog_dev_server): report route conflicts

--- bricks/dart_frog_dev_server/hooks/pre_gen.dart
+++ bricks/dart_frog_dev_server/hooks/pre_gen.dart
@@ -1,16 +1,27 @@
 import 'dart:io';
 
-import 'package:mason/mason.dart';
 import 'package:dart_frog_gen/dart_frog_gen.dart';
+import 'package:mason/mason.dart';
+import 'package:path/path.dart' as path;
 
-Future<void> run(HookContext context) async {
+typedef RouteConfigurationBuilder = RouteConfiguration Function(
+  Directory directory,
+);
+
+Future<void> run(
+  HookContext context, {
+  RouteConfigurationBuilder buildConfiguration = buildRouteConfiguration,
+  void Function(int exitCode) exit = exit,
+}) async {
   final RouteConfiguration configuration;
   try {
-    configuration = buildRouteConfiguration(Directory.current);
+    configuration = buildConfiguration(Directory.current);
   } catch (error) {
     context.logger.err('$error');
-    exit(1);
+    return exit(1);
   }
+
+  reportRouteConflicts(context, configuration);
 
   context.vars = {
     'port': context.vars['port'] ?? '8080',
@@ -26,4 +37,27 @@ Future<void> run(HookContext context) async {
         : false,
     'serveStaticFiles': configuration.serveStaticFiles,
   };
+}
+
+void reportRouteConflicts(
+  HookContext context,
+  RouteConfiguration configuration,
+) {
+  final conflictingEndpoints =
+      configuration.endpoints.entries.where((entry) => entry.value.length > 1);
+  if (conflictingEndpoints.isNotEmpty) {
+    context.logger.info('');
+    for (final conflict in conflictingEndpoints) {
+      final originalFilePath = path.normalize(
+        path.join('routes', conflict.value.first.path),
+      );
+      final conflictingFilePath = path.normalize(
+        path.join('routes', conflict.value.last.path),
+      );
+      context.logger.err(
+        'Route conflict detected. ${lightCyan.wrap(originalFilePath)} and ${lightCyan.wrap(conflictingFilePath)} both resolve to ${lightCyan.wrap(conflict.key)}.',
+      );
+    }
+    context.logger.info('');
+  }
 }
---
+++ bricks/dart_frog_dev_server/hooks/test/pre_gen_test.dart
@@ -0,0 +1,237 @@
+import 'package:dart_frog_gen/dart_frog_gen.dart';
+import 'package:mason/mason.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../pre_gen.dart';
+
+class _MockHookContext extends Mock implements HookContext {}
+
+class _FakeHookContext extends Fake implements HookContext {
+  _FakeHookContext({Logger? logger}) : _logger = logger ?? _MockLogger();
+  final Logger _logger;
+
+  var _vars = <String, dynamic>{};
+
+  @override
+  Map<String, dynamic> get vars => _vars;
+
+  @override
+  set vars(Map<String, dynamic> value) => _vars = value;
+
+  @override
+  Logger get logger => _logger;
+}
+
+class _MockLogger extends Mock implements Logger {}
+
+class _MockRouteConfiguration extends Mock implements RouteConfiguration {}
+
+void main() {
+  group('run', () {
+    late HookContext context;
+    late Logger logger;
+
+    setUp(() {
+      logger = _MockLogger();
+      context = _FakeHookContext(logger: logger);
+    });
+
+    test('exit(1) if buildRouteConfiguration throws', () async {
+      final exitCalls = <int>[];
+      final exception = Exception('oops');
+      await run(
+        context,
+        buildConfiguration: (_) => throw exception,
+        exit: exitCalls.add,
+      );
+      expect(exitCalls, equals([1]));
+      verify(() => logger.err(exception.toString())).called(1);
+    });
+
+    test('retains custom port if specified', () async {
+      const customPort = '8081';
+      context.vars['port'] = customPort;
+      final configuration = RouteConfiguration(
+        middleware: [],
+        directories: [],
+        routes: [],
+        endpoints: {},
+      );
+      final exitCalls = <int>[];
+      await run(
+        context,
+        buildConfiguration: (_) => configuration,
+        exit: exitCalls.add,
+      );
+      expect(exitCalls, isEmpty);
+      verifyNever(() => logger.err(any()));
+      expect(
+        context.vars,
+        equals({
+          'port': customPort,
+          'directories': [],
+          'routes': [],
+          'middleware': [],
+          'globalMiddleware': false,
+          'serveStaticFiles': false,
+        }),
+      );
+    });
+
+    test('updates context.vars when buildRouteConfiguration succeeds',
+        () async {
+      final configuration = RouteConfiguration(
+        globalMiddleware: MiddlewareFile(
+          name: 'middleware',
+          path: 'middleware.dart',
+        ),
+        middleware: [
+          MiddlewareFile(
+            name: 'hello_middleware',
+            path: 'hello/middleware.dart',
+          )
+        ],
+        directories: [
+          RouteDirectory(
+            name: '_',
+            route: '/',
+            middleware: null,
+            files: [
+              RouteFile(name: 'index', path: 'index.dart', route: '/'),
+              RouteFile(name: 'hello', path: 'hello.dart', route: '/hello'),
+            ],
+          )
+        ],
+        routes: [
+          RouteFile(name: 'index', path: 'index.dart', route: '/'),
+          RouteFile(name: 'hello', path: 'hello.dart', route: '/hello'),
+        ],
+        endpoints: {
+          '/': [
+            RouteFile(name: 'index', path: 'index.dart', route: '/'),
+          ],
+          '/hello': [
+            RouteFile(name: 'hello', path: 'hello.dart', route: '/hello'),
+          ]
+        },
+        serveStaticFiles: true,
+      );
+      final exitCalls = <int>[];
+      await run(
+        context,
+        buildConfiguration: (_) => configuration,
+        exit: exitCalls.add,
+      );
+      expect(exitCalls, isEmpty);
+      verifyNever(() => logger.err(any()));
+      expect(
+        context.vars,
+        equals({
+          'port': '8080',
+          'directories': [
+            {
+              'name': '_',
+              'route': '/',
+              'middleware': false,
+              'files': [
+                {'name': 'index', 'path': 'index.dart', 'route': '/'},
+                {'name': 'hello', 'path': 'hello.dart', 'route': '/hello'}
+              ]
+            }
+          ],
+          'routes': [
+            {'name': 'index', 'path': 'index.dart', 'route': '/'},
+            {'name': 'hello', 'path': 'hello.dart', 'route': '/hello'}
+          ],
+          'middleware': [
+            {'name': 'hello_middleware', 'path': 'hello/middleware.dart'}
+          ],
+          'globalMiddleware': {'name': 'middleware', 'path': 'middleware.dart'},
+          'serveStaticFiles': true,
+        }),
+      );
+    });
+  });
+
+  group('reportRouteConflicts', () {
+    late HookContext context;
+    late Logger logger;
+    late RouteConfiguration configuration;
+
+    setUp(() {
+      context = _MockHookContext();
+      logger = _MockLogger();
+      configuration = _MockRouteConfiguration();
+
+      when(() => context.logger).thenReturn(logger);
+    });
+
+    test('reports nothing when there are no endpoints', () {
+      when(() => configuration.endpoints).thenReturn({});
+      reportRouteConflicts(context, configuration);
+      verifyNever(() => logger.err(any()));
+    });
+
+    test('reports nothing when there are endpoints and no conflicts', () {
+      when(() => configuration.endpoints).thenReturn({
+        '/': [
+          RouteFile(name: 'index', path: 'index.dart', route: '/'),
+        ],
+        '/hello': [
+          RouteFile(name: 'hello', path: 'hello.dart', route: '/hello')
+        ]
+      });
+      reportRouteConflicts(context, configuration);
+      verifyNever(() => logger.err(any()));
+    });
+
+    test('reports single conflict when there is one endpoint with conflicts',
+        () {
+      when(() => configuration.endpoints).thenReturn({
+        '/': [
+          RouteFile(name: 'index', path: 'index.dart', route: '/'),
+        ],
+        '/hello': [
+          RouteFile(name: 'hello', path: 'hello.dart', route: '/hello'),
+          RouteFile(name: 'hello_index', path: 'hello/index.dart', route: '/')
+        ]
+      });
+      reportRouteConflicts(context, configuration);
+      verify(
+        () => logger.err(
+          'Route conflict detected. ${lightCyan.wrap('routes/hello.dart')} and ${lightCyan.wrap('routes/hello/index.dart')} both resolve to ${lightCyan.wrap('/hello')}.',
+        ),
+      );
+    });
+
+    test(
+        'reports multiple conflicts when there are multiple endpoint with conflicts',
+        () {
+      when(() => configuration.endpoints).thenReturn({
+        '/': [
+          RouteFile(name: 'index', path: 'index.dart', route: '/'),
+        ],
+        '/hello': [
+          RouteFile(name: 'hello', path: 'hello.dart', route: '/hello'),
+          RouteFile(name: 'hello_index', path: 'hello/index.dart', route: '/')
+        ],
+        '/echo': [
+          RouteFile(name: 'echo', path: 'echo.dart', route: '/echo'),
+          RouteFile(name: 'echo_index', path: 'echo/index.dart', route: '/')
+        ]
+      });
+      reportRouteConflicts(context, configuration);
+      verify(
+        () => logger.err(
+          'Route conflict detected. ${lightCyan.wrap('routes/hello.dart')} and ${lightCyan.wrap('routes/hello/index.dart')} both resolve to ${lightCyan.wrap('/hello')}.',
+        ),
+      );
+      verify(
+        () => logger.err(
+          'Route conflict detected. ${lightCyan.wrap('routes/echo.dart')} and ${lightCyan.wrap('routes/echo/index.dart')} both resolve to ${lightCyan.wrap('/echo')}.',
+        ),
+      );
+    });
+  });
+}
