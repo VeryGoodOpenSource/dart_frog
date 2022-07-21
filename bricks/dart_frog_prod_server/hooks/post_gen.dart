ba4789df82764a88a1f463e76384541f4cad67bc
Felix Angelov <felangelov@gmail.com>
2022-07-21 15:31:27 -0500

    test(dart_frog_prod_server): post_gen tests

--- bricks/dart_frog_prod_server/hooks/post_gen.dart
+++ bricks/dart_frog_prod_server/hooks/post_gen.dart
@@ -1,23 +1,34 @@
-import 'dart:io';
+import 'dart:async';
+import 'dart:io' as io;
 
 import 'package:mason/mason.dart';
 import 'package:path/path.dart' as path;
 
-Future<void> run(HookContext context) async {
-  final progress = context.logger.progress('Installing dependencies');
-  final buildDirectoryPath = path.join(Directory.current.path, 'build');
-  final result = await Process.run(
-    'dart',
-    ['pub', 'get'],
-    workingDirectory: buildDirectoryPath,
-    runInShell: true,
-  );
-  progress.complete();
+typedef ProcessRunner = Future<io.ProcessResult> Function(
+  String executable,
+  List<String> arguments, {
+  String workingDirectory,
+  bool runInShell,
+});
 
-  if (result.exitCode != 0) {
-    context.logger.err('${result.stderr}');
-    exit(result.exitCode);
-  }
+Future<void> run(HookContext context) => postGen(context);
+
+Future<void> postGen(
+  HookContext context, {
+  io.Directory? directory,
+  ProcessRunner runProcess = io.Process.run,
+  void Function(int exitCode)? exit,
+}) async {
+  final _exit = exit ?? ExitOverrides.current?.exit ?? io.exit;
+  final _directory = directory ?? io.Directory.current;
+  final buildDirectoryPath = path.join(_directory.path, 'build');
+
+  await dartPubGet(
+    context,
+    workingDirectory: buildDirectoryPath,
+    runProcess: runProcess,
+    exit: _exit,
+  );
 
   final relativeBuildPath = path.relative(buildDirectoryPath);
   context.logger
@@ -29,4 +40,59 @@ Future<void> run(HookContext context) async {
     ..info(
       '''${lightCyan.wrap('dart ${path.join(relativeBuildPath, 'bin', 'server.dart')}')}''',
     );
+}
+
+Future<void> dartPubGet(
+  HookContext context, {
+  required String workingDirectory,
+  required ProcessRunner runProcess,
+  required void Function(int exitCode) exit,
+}) async {
+  final progress = context.logger.progress('Installing dependencies');
+  try {
+    final result = await runProcess(
+      'dart',
+      ['pub', 'get'],
+      workingDirectory: workingDirectory,
+      runInShell: true,
+    );
+    progress.complete();
+
+    if (result.exitCode != 0) {
+      context.logger.err('${result.stderr}');
+      return exit(result.exitCode);
+    }
+  } on io.ProcessException catch (error) {
+    context.logger.err(error.message);
+    return exit(error.errorCode);
+  }
+}
+
+const _asyncRunZoned = runZoned;
+
+abstract class ExitOverrides {
+  static final _token = Object();
+
+  static ExitOverrides? get current {
+    return Zone.current[_token] as ExitOverrides?;
+  }
+
+  static R runZoned<R>(R Function() body, {void Function(int)? exit}) {
+    final overrides = _ExitOverridesScope(exit);
+    return _asyncRunZoned(body, zoneValues: {_token: overrides});
+  }
+
+  void Function(int exitCode) get exit => io.exit;
+}
+
+class _ExitOverridesScope extends ExitOverrides {
+  _ExitOverridesScope(this._exit);
+
+  final ExitOverrides? _previous = ExitOverrides.current;
+  final void Function(int exitCode)? _exit;
+
+  @override
+  void Function(int exitCode) get exit {
+    return _exit ?? _previous?.exit ?? super.exit;
+  }
 }
