9cc88dfaa8b9a84e6066c19db6f824dafa71984c
Felix Angelov <felangelov@gmail.com>
2022-08-01 10:23:26 -0500

    format

--- packages/dart_frog_cli/lib/src/commands/create/create.dart
+++ packages/dart_frog_cli/lib/src/commands/create/create.dart
@@ -46,7 +46,7 @@ class CreateCommand extends DartFrogCommand {
       'name': projectName,
       'output_directory': outputDirectory.absolute.path
     };
-    
+
     logger.detail('[codegen] running generate...');
     final _ = await generator.generate(
       DirectoryGeneratorTarget(outputDirectory),
