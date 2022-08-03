854e040de968fa8a1162901558c35e71d2244fbd
Felix Angelov <felangelov@gmail.com>
2022-08-03 16:55:40 -0500

    chore: remove old example

--- example/routes/_middleware.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-
-Handler middleware(Handler handler) {
-  return handler
-      .use(requestLogger())
-      .use(provider<String>((_) => 'Welcome to Dart Frog!'));
-}
