72c933bbe25a29451cd31bdbc152264684f055e4
Felix Angelov <felangelov@gmail.com>
2022-09-21 14:32:22 -0700

    feat(dart_frog_dev): detect external path dependencies

--- bricks/dart_frog_dev_server/hooks/pre_gen.dart
+++ bricks/dart_frog_dev_server/hooks/pre_gen.dart
@@ -4,6 +4,7 @@ import 'dart:io' as io;
 import 'package:dart_frog_gen/dart_frog_gen.dart';
 import 'package:mason/mason.dart';
 import 'package:path/path.dart' as path;
+import 'package:pubspec_parse/pubspec_parse.dart';
 
 typedef RouteConfigurationBuilder = RouteConfiguration Function(
   io.Directory directory,
@@ -13,6 +14,7 @@ Future<void> run(HookContext context) async => preGen(context);
 
 Future<void> preGen(
   HookContext context, {
+  io.Directory? directory,
   RouteConfigurationBuilder buildConfiguration = buildRouteConfiguration,
   void Function(int exitCode)? exit,
 }) async {
@@ -25,8 +27,11 @@ Future<void> preGen(
     return _exit(1);
   }
 
+  final projectDirectory = directory ?? io.Directory.current;
+
   reportRouteConflicts(context, configuration);
   reportRogueRoutes(context, configuration);
+  await reportExternalPathDependencies(context, projectDirectory);
 
   context.vars = {
     'port': context.vars['port'] ?? '8080',
@@ -43,6 +48,39 @@ Future<void> preGen(
     'serveStaticFiles': configuration.serveStaticFiles,
     'invokeCustomEntrypoint': configuration.invokeCustomEntrypoint,
   };
+}
+
+Future<void> reportExternalPathDependencies(
+  HookContext context,
+  io.Directory directory,
+) async {
+  final pubspec = Pubspec.parse(
+    await io.File(path.join(directory.path, 'pubspec.yaml')).readAsString(),
+  );
+
+  final dependencies = pubspec.dependencies;
+  final devDependencies = pubspec.devDependencies;
+  final pathDependencies = [...dependencies.entries, ...devDependencies.entries]
+      .where((entry) => entry.value is PathDependency)
+      .map((entry) {
+    final value = entry.value as PathDependency;
+    return [entry.key, value.path];
+  }).toList();
+  final externalDependencies = pathDependencies.where(
+    (dep) => !path.isWithin(directory.path, dep.last),
+  );
+
+  if (externalDependencies.isNotEmpty) {
+    context.logger
+      ..info('')
+      ..err('All path dependencies must be within the project.')
+      ..err('External path dependencies detected:');
+    for (final dependency in externalDependencies) {
+      final dependencyName = dependency.first;
+      final dependencyPath = path.normalize(dependency.last);
+      context.logger.err('  \u{2022} $dependencyName from $dependencyPath');
+    }
+  }
 }
 
 void reportRouteConflicts(
