581b46bf7a8566f1ddaddcf81517a0f97a11488a
Felix Angelov <felangelov@gmail.com>
2022-09-28 18:34:59 -0500

    feat(dart_frog_cli): update prompt and command (#348)

---
+++ packages/dart_frog_cli/lib/src/commands/update/update.dart
@@ -0,0 +1,58 @@
+import 'package:args/command_runner.dart';
+import 'package:dart_frog_cli/src/command_runner.dart';
+import 'package:dart_frog_cli/src/version.dart';
+import 'package:mason/mason.dart' hide packageVersion;
+import 'package:pub_updater/pub_updater.dart';
+
+/// {@template update_command}
+/// `dart_frog update` command which updates the dart_frog_cli.
+/// {@endtemplate}
+class UpdateCommand extends Command<int> {
+  /// {@macro update_command}
+  UpdateCommand({
+    required Logger logger,
+    PubUpdater? pubUpdater,
+  })  : _logger = logger,
+        _pubUpdater = pubUpdater ?? PubUpdater();
+
+  final Logger _logger;
+  final PubUpdater _pubUpdater;
+
+  @override
+  String get description => 'Update the Dart Frog CLI.';
+
+  @override
+  String get name => 'update';
+
+  @override
+  Future<int> run() async {
+    final updateCheckProgress = _logger.progress('Checking for updates');
+    late final String latestVersion;
+    try {
+      latestVersion = await _pubUpdater.getLatestVersion(packageName);
+    } catch (error) {
+      updateCheckProgress.fail();
+      _logger.err('$error');
+      return ExitCode.software.code;
+    }
+    updateCheckProgress.complete('Checked for updates');
+
+    final isUpToDate = packageVersion == latestVersion;
+    if (isUpToDate) {
+      _logger.info('$packageName is already at the latest version.');
+      return ExitCode.success.code;
+    }
+
+    final updateProgress = _logger.progress('Updating to $latestVersion');
+    try {
+      await _pubUpdater.update(packageName: packageName);
+    } catch (error) {
+      updateProgress.fail();
+      _logger.err('$error');
+      return ExitCode.software.code;
+    }
+    updateProgress.complete('Updated to $latestVersion');
+
+    return ExitCode.success.code;
+  }
+}
