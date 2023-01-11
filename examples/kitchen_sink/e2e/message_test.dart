e12badb2aece788d6acbdc966b00311179ed4ffe
Felix Angelov <felangelov@gmail.com>
2023-01-11 15:11:27 -0600

    add e2e tests

---
+++ examples/kitchen_sink/e2e/message_test.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
+
+void main() {
+  group('E2E (/messages)', () {
+    test('GET /messages responds with 405', () async {
+      final response = await http.get(
+        Uri.parse('http://localhost:8080/messages'),
+      );
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
+    });
+
+    test('POST /messages responds with 400 when body is empty', () async {
+      final response = await http.post(
+        Uri.parse('http://localhost:8080/messages'),
+      );
+      expect(response.statusCode, equals(HttpStatus.badRequest));
+    });
+
+    test('POST /messages responds with 200 when body is not empty', () async {
+      const message = 'hello world';
+      final response = await http.post(
+        Uri.parse('http://localhost:8080/messages'),
+        body: message,
+      );
+      expect(response.statusCode, equals(HttpStatus.ok));
+      expect(response.body, equals('message: $message'));
+    });
+  });
+}
