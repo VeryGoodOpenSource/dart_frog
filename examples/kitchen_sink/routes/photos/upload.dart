afe0f3cb9debb4dced7a802fa8bace880efd0a7a
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2023-02-28 20:59:49 +0100

    feat: multipart form data

--- examples/kitchen_sink/routes/photos/upload.dart
+++ examples/kitchen_sink/routes/photos/upload.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
+final contentTypePng = ContentType('image', 'png');
+
 Future<Response> onRequest(RequestContext context) async {
-  final data = await context.request.formData();
-  return Response.json(body: data);
+  final formData = await context.request.formData();
+  final photo = formData.files['photo'];
+
+  if (photo == null || photo.contentType.mimeType != contentTypePng.mimeType) {
+    return Response(statusCode: HttpStatus.badRequest);
+  }
+
+  return Response.json(
+    body: {'message': 'Successfully uploaded ${photo.name}'},
+  );
 }
---
+++ examples/kitchen_sink/test/routes/photos/upload_test.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../../routes/photos/upload.dart' as route;
+
+class _MockRequestContext extends Mock implements RequestContext {}
+
+class _MockRequest extends Mock implements Request {}
+
+void main() {
+  group('POST /upload', () {
+    test('responds with a 400', () async {
+      final context = _MockRequestContext();
+      final request = _MockRequest();
+      when(() => context.request).thenReturn(request);
+
+      final formData = FormData({}, {
+        'photo': UploadedFile(
+          'file.txt',
+          ContentType.text,
+          Stream.fromIterable([[]]),
+        )
+      });
+      when(request.formData).thenAnswer((_) async => formData);
+
+      final response = await route.onRequest(context);
+      expect(response.statusCode, equals(HttpStatus.badRequest));
+    });
+
+    test('responds with a 200', () async {
+      final context = _MockRequestContext();
+      final request = _MockRequest();
+      when(() => context.request).thenReturn(request);
+
+      final formData = FormData({}, {
+        'photo': UploadedFile(
+          'picture.png',
+          ContentType('image', 'png'),
+          Stream.fromIterable([[]]),
+        )
+      });
+      when(request.formData).thenAnswer((_) async => formData);
+
+      final response = await route.onRequest(context);
+      expect(response.statusCode, equals(HttpStatus.ok));
+      expect(
+        response.json(),
+        completion(
+          equals({'message': 'Successfully uploaded picture.png'}),
+        ),
+      );
+    });
+  });
+}
