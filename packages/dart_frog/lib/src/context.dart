ebd52c25db5245ab3dd8c9e44dfa75bfec41c146
Felix Angelov <felix@shorebird.dev>
2023-03-17 14:50:03 -0500

    fix(dart_frog): allow providing nullable objects

--- packages/dart_frog/lib/src/context.dart
+++ packages/dart_frog/lib/src/context.dart
@@ -11,7 +11,7 @@ class RequestContext {
 
   /// Provide the value returned by [create] to the respective
   /// request context.
-  RequestContext provide<T extends Object>(T Function() create) {
+  RequestContext provide<T extends Object?>(T Function() create) {
     return RequestContext._(
       request._request.change(
         context: {...request._request.context, '$T': create},
@@ -24,8 +24,7 @@ class RequestContext {
   /// A [StateError] is thrown if [T] is not available within the
   /// provided [request] context.
   T read<T>() {
-    final value = request._request.context['$T'];
-    if (value == null) {
+    if (!request._request.context.containsKey('$T')) {
       throw StateError(
         '''
 context.read<$T>() called with a request context that does not contain a $T.
@@ -40,6 +39,8 @@ This can happen if $T was not provided to the request context:
 ''',
       );
     }
-    return (value as T Function())();
+    final value = request._request.context['$T']!;
+    final create = value as T Function();
+    return create();
   }
 }
