b0114d9597ae1649caa3e73018788d8b4429c9b9
Remi Rousselet <darky12s@gmail.com>
2023-04-19 15:32:11 +0200

    Rename dart_frog_route -> dart_frog_request

--- packages/dart_frog_lint/lib/dart_frog_lint.dart
+++ packages/dart_frog_lint/lib/dart_frog_lint.dart
@@ -1,7 +1,7 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:dart_frog_lint/src/dart_frog_entrypoint.dart';
 import 'package:dart_frog_lint/src/dart_frog_middleware.dart';
-import 'package:dart_frog_lint/src/dart_frog_route.dart';
+import 'package:dart_frog_lint/src/dart_frog_request.dart';
 
 /// The entrypoint of dart_frog_lint
 PluginBase createPlugin() => _DartFrogLintPlugin();
