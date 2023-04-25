ab5061998d948b71b2a265f8da5ae7f66417c33e
Alejandro Santiago <dev@alestiago.com>
2023-04-25 19:37:11 +0100

    test: killing conditional for MacOS

--- packages/dart_frog_cli/e2e/test/dev/dart_frog_multiple_dev_test.dart
+++ packages/dart_frog_cli/e2e/test/dev/dart_frog_multiple_dev_test.dart
@@ -36,7 +36,8 @@ void main() {
 
     tearDownAll(() async {
       await killDartFrogServer(process1.pid, port: port1);
-      await killDartFrogServer(process2.pid, port: port2);
+      if (!Platform.isMacOS)
+        await killDartFrogServer(process2.pid, port: port2);
       tempDirectory.delete(recursive: true).ignore();
     });
 
