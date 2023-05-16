f10004efb3a5ef3d9674cb5db9b508f75424c1f3
Renan <6718144+renancaraujo@users.noreply.github.com>
2023-05-16 13:21:32 +0100

    feat(dart_frog_cli): add dart_frog_new_brick (middleware implementation) (#624)
    * feat(dart_frog_cli): add dart_frog_new_brick
    
    * Update bricks/dart_frog_new/__brick__/{{~ middleware.dart }}

--- bricks/dart_frog_new/hooks/lib/pre_gen.dart
+++ bricks/dart_frog_new/hooks/lib/pre_gen.dart
@@ -46,16 +46,31 @@ void preGen(
     return exit(1);
   }
 
-  // The path in which the route will be created
+  // The path in which the route or middleware will be created
   final routePath = normalizeRoutePath(context.vars['route_path'] as String);
 
-  return _preGenRoute(
-    context,
-    routePath: routePath,
-    routeConfiguration: routeConfiguration,
-    projectDirectory: projectDirectory,
-    exit: exit,
-  );
+  if (type == 'route') {
+    return _preGenRoute(
+      context,
+      routePath: routePath,
+      routeConfiguration: routeConfiguration,
+      projectDirectory: projectDirectory,
+      exit: exit,
+    );
+  }
+
+  if (type == 'middleware') {
+    return _preGenMiddleware(
+      context,
+      routePath: routePath,
+      routeConfiguration: routeConfiguration,
+      projectDirectory: projectDirectory,
+      exit: exit,
+    );
+  }
+
+  context.logger.err('Unrecognized type: $type');
+  return exit(1);
 }
 
 void _preGenRoute(
@@ -125,7 +140,86 @@ void _preGenRoute(
     return exit(1);
   }
 
+  context.vars['is_route'] = true;
   context.vars['dir_path'] = path.dirname(routeFileName);
   context.vars['filename'] = path.basename(routeFileName);
   context.vars['params'] = parameterNames;
+}
+
+void _preGenMiddleware(
+  HookContext context, {
+  required String routePath,
+  required RouteConfiguration routeConfiguration,
+  required io.Directory projectDirectory,
+  required void Function(int exitCode) exit,
+}) {
+  final routesDirectoryPath = path.relative(
+    io.Directory(path.join(projectDirectory.path, 'routes')).path,
+  );
+
+  const middlewareFilename = '_middleware.dart';
+
+  // Get the path to directory containing the middleware file
+  final String middlewareContainingDir;
+  if (routePath == '/') {
+    middlewareContainingDir = routesDirectoryPath;
+  } else {
+    middlewareContainingDir = path.withoutExtension(
+      routeToPath(
+        routePath.toBracketParameterSyntax,
+        preamble: routesDirectoryPath,
+      ),
+    );
+  }
+
+  // Verify if the middleware file already exists
+  final middlewareFilePath =
+      path.join(middlewareContainingDir, middlewareFilename);
+  final middlewareExists = io.File(middlewareFilePath).existsSync();
+  if (middlewareExists) {
+    context.logger.err('There is already a middleware at $middlewareFilePath');
+    return exit(1);
+  }
+
+  // Verify if the given route already exists as directory
+  final routeExistsAsDirectory = io.Directory(
+    middlewareContainingDir.toBracketParameterSyntax,
+  ).existsSync();
+
+  // If the route does not exist as directory, we must check if any of its
+  // ancestor routes exists as file routes to avoid rogue routes.
+  if (!routeExistsAsDirectory) {
+    final fileRoute = routeConfiguration.containingFileRoute(
+      routePath,
+      includeSelf: true,
+    );
+
+    if (fileRoute != null) {
+      final filePath = path.normalize(
+        path.join(routesDirectoryPath, fileRoute.path),
+      );
+
+      io.Directory(path.withoutExtension(filePath)).createSync();
+
+      final newFilepath = filePath.replaceFirst('.dart', '/index.dart');
+      io.File(filePath).renameSync(newFilepath);
+      context.logger
+          .detail('Renamed $filePath to $newFilepath to avoid rogue routes');
+    }
+  }
+
+  try {
+    middlewareContainingDir.toBracketParameterSyntax.getParameterNames();
+  } on FormatException catch (exception) {
+    context.logger.err('Failed to create middleware: ${exception.message}');
+    return exit(1);
+  }
+
+  context.logger.detail(
+    'Creating middleware file: ${middlewareFilePath.toBracketParameterSyntax}',
+  );
+
+  context.vars['is_middleware'] = true;
+  context.vars['dir_path'] = middlewareContainingDir.toBracketParameterSyntax;
+  context.vars['filename'] = middlewareFilename;
 }
--- bricks/dart_frog_new/hooks/test/pre_gen_test.dart
+++ bricks/dart_frog_new/hooks/test/pre_gen_test.dart
@@ -179,15 +179,33 @@ void main() {
       expect(exitCalls, equals([1]));
     });
 
+    test('exit(1) for invalid type', () {
+      final exitCalls = <int>[];
+
+      context.vars['type'] = 'invalid';
+      context.vars['route_path'] = '/[id]/existing_as_dir';
+      preGen(
+        context,
+        buildConfiguration: (_) => validRouteConfiguration,
+        exit: exitCalls.add,
+      );
+      verify(
+        () => logger.err(
+          '''Unrecognized type: invalid''',
+        ),
+      );
+      expect(exitCalls, equals([1]));
+    });
+
     group('Type: route', () {
       late io.Directory directory;
       late List<int> exitCalls;
       setUp(() {
-        context.vars['type'] = 'route';
         directory = io.Directory.systemTemp.createTempSync(
           'dart_frog_new_hooks_test',
         );
         exitCalls = <int>[];
+        context.vars['type'] = 'route';
       });
       tearDown(() {
         directory.deleteSync(recursive: true);
@@ -286,6 +304,7 @@ void main() {
         );
         expect(context.vars['filename'], 'new_route.dart');
         expect(context.vars['params'], ['id']);
+        expect(context.vars['is_route'], true);
 
         expect(exitCalls, isEmpty);
       });
@@ -312,6 +331,7 @@ void main() {
           );
           expect(context.vars['filename'], 'index.dart');
           expect(context.vars['params'], ['id']);
+          expect(context.vars['is_route'], true);
 
           expect(exitCalls, isEmpty);
         },
@@ -336,6 +356,187 @@ void main() {
 
         expect(context.vars['filename'], 'new_route.dart');
         expect(context.vars['params'], ['id', 'a']);
+        expect(context.vars['is_route'], true);
+
+        expect(exitCalls, isEmpty);
+      });
+    });
+
+    group('Type: middleware', () {
+      setUp(() {
+        context.vars['type'] = 'middleware';
+      });
+
+      test('exit(1) if middleware already exists (global)', () {
+        final exitCalls = <int>[];
+
+        final directory = io.Directory.systemTemp.createTempSync(
+          'dart_frog_new_hooks_test',
+        );
+        addTearDown(() {
+          directory.deleteSync(recursive: true);
+        });
+        final filePath = path.join(
+          directory.path,
+          'routes',
+          '_middleware.dart',
+        );
+        io.File(filePath)
+          ..createSync(recursive: true)
+          ..writeAsStringSync('content');
+
+        context.vars['route_path'] = '/';
+        preGen(
+          context,
+          buildConfiguration: (_) => validRouteConfiguration,
+          exit: exitCalls.add,
+          directory: directory,
+        );
+
+        verify(
+          () => logger.err(
+            '''There is already a middleware at ${path.relative(filePath)}''',
+          ),
+        );
+        expect(exitCalls, equals([1]));
+      });
+
+      test('exit(1) if middleware already exists (local)', () {
+        final exitCalls = <int>[];
+
+        final directory = io.Directory.systemTemp.createTempSync(
+          'dart_frog_new_hooks_test',
+        );
+        addTearDown(() {
+          directory.deleteSync(recursive: true);
+        });
+        final filePath =
+            path.join(directory.path, 'routes', '[id]', '_middleware.dart');
+        io.File(filePath)
+          ..createSync(recursive: true)
+          ..writeAsStringSync('content');
+
+        context.vars['route_path'] = '/[id]';
+        preGen(
+          context,
+          buildConfiguration: (_) => validRouteConfiguration,
+          exit: exitCalls.add,
+          directory: directory,
+        );
+
+        verify(
+          () => logger.err(
+            '''There is already a middleware at ${path.relative(filePath)}''',
+          ),
+        );
+        expect(exitCalls, equals([1]));
+      });
+
+      test('exit(1) if route path has duplicate parameter names', () {
+        final exitCalls = <int>[];
+        context.vars['route_path'] = '/[id]/[id]';
+
+        final directory = io.Directory.systemTemp.createTempSync(
+          'dart_frog_new_hooks_test',
+        );
+        addTearDown(() {
+          directory.deleteSync(recursive: true);
+        });
+
+        preGen(
+          context,
+          buildConfiguration: (_) => validRouteConfiguration,
+          exit: exitCalls.add,
+          directory: directory,
+        );
+
+        verify(
+          () => logger.err(
+            '''Failed to create middleware: Duplicate parameter name found: id''',
+          ),
+        );
+        expect(exitCalls, equals([1]));
+      });
+
+      test('Renames a wrapping route that exists as file to an index', () {
+        final exitCalls = <int>[];
+
+        final directory = io.Directory.systemTemp.createTempSync(
+          'dart_frog_new_hooks_test',
+        );
+        addTearDown(() {
+          directory.deleteSync(recursive: true);
+        });
+        final filePath = path.join(
+          directory.path,
+          'routes',
+          '[id]',
+          'existing_as_file.dart',
+        );
+        io.File(filePath)
+          ..createSync(recursive: true)
+          ..writeAsStringSync('content');
+
+        context.vars['route_path'] = '/[id]/existing_as_file';
+
+        preGen(
+          context,
+          buildConfiguration: (_) => validRouteConfiguration,
+          exit: exitCalls.add,
+          directory: directory,
+        );
+
+        expect(io.File(filePath).existsSync(), isFalse);
+
+        final newFilepath = path.join(
+          directory.path,
+          'routes',
+          '[id]',
+          'existing_as_file',
+          'index.dart',
+        );
+
+        expect(io.File(newFilepath).readAsStringSync(), equals('content'));
+        expect(
+          context.vars['dir_path'],
+          path.relative(
+            path.join(directory.path, 'routes', '[id]', 'existing_as_file'),
+          ),
+        );
+        expect(context.vars['filename'], '_middleware.dart');
+        expect(context.vars['is_middleware'], true);
+
+        expect(exitCalls, isEmpty);
+      });
+
+      test('Sets up a middleware path correctly', () {
+        final exitCalls = <int>[];
+
+        final directory = io.Directory.systemTemp.createTempSync(
+          'dart_frog_new_hooks_test',
+        );
+        addTearDown(() {
+          directory.deleteSync(recursive: true);
+        });
+
+        context.vars['route_path'] = '/[id]/existing_as_dir';
+        preGen(
+          context,
+          buildConfiguration: (_) => validRouteConfiguration,
+          exit: exitCalls.add,
+          directory: directory,
+        );
+
+        expect(
+          context.vars['dir_path'],
+          path.relative(
+            path.join(directory.path, 'routes', '[id]', 'existing_as_dir'),
+          ),
+        );
+        expect(context.vars['filename'], '_middleware.dart');
+        expect(context.vars['is_middleware'], true);
+
+        expect(exitCalls, isEmpty);
       });
     });
   });
