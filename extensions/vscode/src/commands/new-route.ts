127aa8bc94be723b99e63979bdad15044fd9c980
Alejandro Santiago <dev@alestiago.com>
2023-06-09 23:42:46 +0100

    feat: some progess

--- extensions/vscode/src/commands/new-route.ts
+++ extensions/vscode/src/commands/new-route.ts
@@ -1,3 +1,6 @@
+const cp = require("child_process");
+const path = require("node:path");
+
 import {
   InputBoxOptions,
   OpenDialogOptions,
@@ -8,6 +11,17 @@ import {
 
 export const newRoute = async (uri: Uri) => {
   const routeName = await getRouteName();
+  if (routeName === undefined || routeName.trim() === "") {
+    return;
+  }
+
+  const workingDirectory = uri.fsPath;
+
+  // TODO(alestiago): Create issue in dart_frog new to allow creating a new route
+  // outside Dart Frog directory, and remove this workaround.
+  let dartProjectDirectory = path.parse(workingDirectory);
+
+  runDartFrogNewCommand(routeName, workingDirectory);
 };
 
 /**
@@ -16,9 +30,32 @@ export const newRoute = async (uri: Uri) => {
  * @returns { Thenable<string | undefined>} routeName
  */
 function getRouteName(): Thenable<string | undefined> {
-  const blocNamePromptOptions: InputBoxOptions = {
+  const inputBoxOptions: InputBoxOptions = {
     prompt: "Route name",
     placeHolder: "index",
   };
-  return window.showInputBox(blocNamePromptOptions);
+  return window.showInputBox(inputBoxOptions);
+}
+
+/**
+ * Runs the `dart_frog new` command with the given route name.
+ * @param {string} routeName
+ * @param {String} workingDirectory
+ * @returns {void}
+ */
+function runDartFrogNewCommand(routeName: String, workingDirectory: String) {
+  const command = `dart_frog new route ${routeName}`;
+
+  cp.exec(
+    command,
+    {
+      cwd: workingDirectory,
+    },
+    function (error: Error, stdout: String, stderr: String) {
+      if (error) {
+        // TODO(alestiago): Parse error message and show it in a more user-friendly way.
+        window.showErrorMessage(stderr.toString());
+      }
+    }
+  );
 }
