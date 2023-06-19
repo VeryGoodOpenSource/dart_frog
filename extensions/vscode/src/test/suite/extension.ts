da71e6b4e1cc460bb1da1f177f7dc212e6bd3c22
Alejandro Santiago <dev@alestiago.com>
2023-06-19 09:02:42 +0100

    test: initial setup

---
+++ extensions/vscode/src/test/suite/extension.ts
@@ -0,0 +1,18 @@
+import * as assert from "assert";
+import { after } from "mocha";
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from "vscode";
+// import * as myExtension from '../extension';
+
+suite("Extension Test Suite", () => {
+  after(() => {
+    vscode.window.showInformationMessage("All tests done!");
+  });
+
+  test("Sample test", () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+});
