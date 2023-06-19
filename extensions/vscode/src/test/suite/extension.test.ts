da182be00bd8a235c3b4ca46d730c122b1965131
Alejandro Santiago <dev@alestiago.com>
2023-06-19 12:46:31 +0100

    test: ammended test name

--- extensions/vscode/src/test/suite/extension.test.ts
+++ extensions/vscode/src/test/suite/extension.test.ts
@@ -3,7 +3,7 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 
 suite("activate", () => {
-  test("subcribes to one command", async () => {
+  test("subcribes to one disposable", async () => {
     const ext = vscode.extensions.getExtension(
       "VeryGoodVentures.dart-frog"
     ) as vscode.Extension<any>;
