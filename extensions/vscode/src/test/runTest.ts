cd672ce7190b04dcaed88f2beaaaffa9ac0a818b
Alejandro Santiago <dev@alestiago.com>
2023-06-19 09:13:24 +0100

    feat: custom runTest

--- extensions/vscode/src/test/runTest.ts
+++ extensions/vscode/src/test/runTest.ts
@@ -1,16 +1,28 @@
+import * as cp from "child_process";
 import * as path from "path";
-
-import { runTests } from "@vscode/test-electron";
+import {
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+  runTests,
+} from "@vscode/test-electron";
 
 async function main() {
   try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
-
-    // The path to the extension test runner script
-    // Passed to --extensionTestsPath
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../../");
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+    const vscodeExecutablePath = await downloadAndUnzipVSCode("1.79.2");
+    const [cliPath, ...args] =
+      resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    // Package extension
+    cp.spawnSync("vsce package --out extension.vsix");
+    const vsixPath = path.resolve(__dirname, "../../", "extension.vsix");
+
+    // Install extension
+    cp.spawnSync(cliPath, [...args, "--install-extension", vsixPath], {
+      encoding: "utf-8",
+      stdio: "inherit",
+    });
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
