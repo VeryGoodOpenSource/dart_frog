a72787da3769817d6cd8e0375be197cc3f782aa8
Alejandro Santiago <dev@alestiago.com>
2023-06-21 06:31:14 +0100

    chore: registered new-route command on activation

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -1,11 +1,20 @@
 import * as vscode from "vscode";
+import { newRoute } from "./commands";
 
 /**
  * This method is called when the extension is activated.
  *
  * The extension is activated the very first time the command is executed.
+ *
  * @param context
+ * @returns {vscode.ExtensionContext}
  */
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.new-route", newRoute)
+  );
+
+  return context;
+}
 
 export function deactivate() {}
