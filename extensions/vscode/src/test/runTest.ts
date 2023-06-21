ae4a1c9dcbed4f4eb052a3df588606f2311bef9b
Alejandro Santiago <dev@alestiago.com>
2023-06-21 11:28:02 +0100

    test: simplified runTest

--- extensions/vscode/src/test/runTest.ts
+++ extensions/vscode/src/test/runTest.ts
@@ -8,31 +8,13 @@
  * @see https://github.com/prettier/prettier-vscode/blob/main/src/test/runTests.ts
  */
 
-import * as cp from "child_process";
 import * as path from "path";
-import {
-  downloadAndUnzipVSCode,
-  resolveCliArgsFromVSCodeExecutablePath,
-  runTests,
-} from "@vscode/test-electron";
+import { runTests } from "@vscode/test-electron";
 
 async function main() {
   try {
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-    const vscodeExecutablePath = await downloadAndUnzipVSCode("1.79.2");
-    const [cliPath, ...args] =
-      resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
-
-    // Install extension
-    cp.spawnSync(
-      cliPath,
-      [...args, "--install-extension", "VeryGoodVentures.dart-frog"],
-      {
-        encoding: "utf-8",
-        stdio: "inherit",
-      }
-    );
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
