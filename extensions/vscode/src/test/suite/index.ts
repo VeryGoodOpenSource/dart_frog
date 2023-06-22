f8979f11c546885c29da960c466981493e862a71
Alejandro Santiago <dev@alestiago.com>
2023-06-22 15:58:39 +0100

    feat(vscode): includes extension testing setup (#717)
    * feat: includes vscode test setup
    
    * chore: emptied contributes
    
    * chore: removed nyc
    
    * test: updated runTest.ts to use id over vsix
    
    * test: simplified runTest
    
    * docs: removed old doc
    
    * docs: defined return type
    
    * chore: removed redundant catch
    
    * docs: documented return type

---
+++ extensions/vscode/src/test/suite/index.ts
@@ -0,0 +1,43 @@
+/**
+ * This file is used to configure and programmatically runs the test suite.
+ *
+ * @see https://code.visualstudio.com/api/working-with-extensions/testing-extension#the-test-runner-script
+ */
+
+import * as path from "path";
+import * as Mocha from "mocha";
+import * as glob from "glob";
+
+export function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: "tdd",
+    color: true,
+  });
+
+  const testsRoot = path.resolve(__dirname, "..");
+
+  return new Promise((c, e) => {
+    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
+
+      // Add files to the test suite
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run((failures) => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        e(err);
+      }
+    });
+  });
+}
