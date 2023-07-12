6425d06d5d77acf76fa9ca447fdc06aa9316ca05
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-07-12 12:57:44 +0100

    feat: start daemon implementation

---
+++ packages/dart_frog_cli/lib/src/daemon/connection.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_frog_cli/src/daemon/daemon.dart';
+import 'package:meta/meta.dart';
+
+/// {@template daemon_connection}
+/// A connection of the daemon that hooks into an mean of communication with
+/// clients.
+/// {@endtemplate}
+///
+/// See also:
+/// - [DaemonStdioConnection], a connection that hooks into the stdio.
+abstract interface class DaemonConnection {
+  /// A stream of [DaemonMessage]s that are received from the client.
+  Stream<DaemonMessage> get inputStream;
+
+  /// A sink of [DaemonMessage]s that are to be sent to the client.
+  StreamSink<DaemonMessage> get outputSink;
+
+  /// Closes the connection and free resources.
+  Future<void> dispose();
+}
+
+/// {@template daemon_stdio_connection}
+/// A [DaemonConnection] that hooks into the stdio.
+///
+/// This is the default connection used by the daemon.
+///
+/// This uses JSON rpc over stdio to communicate with the client.
+/// {@endtemplate}
+class DaemonStdioConnection implements DaemonConnection {
+  /// {@macro daemon_stdio_connection}
+  DaemonStdioConnection({
+    @visibleForTesting StreamSink<List<int>>? testStdout,
+    @visibleForTesting Stream<List<int>>? testStdin,
+  })  : _stdout = testStdout ?? stdout,
+        _stdin = testStdin ?? stdin {
+    _outputStreamController.stream.listen((message) {
+      final json = jsonEncode(message.toJson());
+      _stdout.add(utf8.encode('[$json]\n'));
+    });
+
+    StreamSubscription<DaemonMessage>? stdinSubscription;
+
+    _inputStreamController
+      ..onListen = () {
+        stdinSubscription = _stdin.readMessages().listen(
+          (message) {
+            _inputStreamController.add(message);
+          },
+          onError: (dynamic error) {
+            switch (error) {
+              case DartFrogDaemonMessageException(message: final message):
+                outputSink.add(
+                  DaemonEvent(
+                    domain: DaemonDomain.name,
+                    event: 'protocolError',
+                    params: {'message': message},
+                  ),
+                );
+              case FormatException(message: _):
+                outputSink.add(
+                  const DaemonEvent(
+                    domain: DaemonDomain.name,
+                    event: 'protocolError',
+                    params: {'message': 'Not a valid JSON'},
+                  ),
+                );
+            }
+          },
+        );
+      }
+      ..onCancel = () {
+        stdinSubscription?.cancel();
+      };
+  }
+
+  final StreamSink<List<int>> _stdout;
+  final Stream<List<int>> _stdin;
+
+  late final StreamController<DaemonMessage> _inputStreamController =
+      StreamController<DaemonMessage>();
+
+  late final StreamController<DaemonMessage> _outputStreamController =
+      StreamController<DaemonMessage>();
+
+  @override
+  Stream<DaemonMessage> get inputStream => _inputStreamController.stream;
+
+  @override
+  StreamSink<DaemonMessage> get outputSink => _outputStreamController.sink;
+
+  @override
+  Future<void> dispose() async {
+    await _inputStreamController.close();
+    await _outputStreamController.close();
+  }
+}
+
+extension on Stream<List<int>> {
+  Stream<DaemonMessage> readMessages() {
+    return transform(utf8.decoder).transform(const LineSplitter()).map((event) {
+      final json = jsonDecode(event);
+      if (json case final List<dynamic> jsonList) {
+        if (jsonList.elementAtOrNull(0)
+            case final Map<String, dynamic> jsonMap) {
+          return DaemonMessage.fromJson(jsonMap);
+        }
+      } else {
+        throw const DartFrogDaemonMessageException(
+          'Message should be placed within a JSON list',
+        );
+      }
+      throw DartFrogDaemonMessageException('Invalid message: $event');
+    });
+  }
+}
---
+++ packages/dart_frog_cli/test/src/daemon/daemon_server_test.dart
@@ -0,0 +1,151 @@
+import 'dart:async';
+
+import 'package:dart_frog_cli/src/daemon/daemon.dart';
+import 'package:mason/mason.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:test/test.dart';
+
+class _MockDaemonConnection extends Mock implements DaemonConnection {}
+
+class _TestDomain extends Domain {
+  _TestDomain() {
+    addHandler('something', _something);
+  }
+
+  @override
+  final String domainName = 'test';
+
+  Future<DaemonResponse> _something(DaemonRequest request) async {
+    return DaemonResponse.success(
+      id: request.id,
+      result: {
+        'foo': 'bar',
+        if (request.params != null) ...request.params!,
+      },
+    );
+  }
+}
+
+void main() {
+  group('$DaemonServer', () {
+    late DaemonConnection connection;
+    late DaemonServer daemonServer;
+    late StreamController<DaemonMessage> inputStreamController;
+    late StreamController<DaemonMessage> outputStreamController;
+
+    late List<DaemonMessage> outputMessages;
+
+    setUp(() {
+      connection = _MockDaemonConnection();
+
+      inputStreamController = StreamController<DaemonMessage>.broadcast();
+      outputStreamController = StreamController<DaemonMessage>.broadcast();
+      when(() => connection.inputStream).thenAnswer(
+        (_) => inputStreamController.stream,
+      );
+      when(() => connection.outputSink).thenAnswer(
+        (_) => outputStreamController.sink,
+      );
+
+      daemonServer = DaemonServer(
+        connection: connection,
+      );
+
+      outputMessages = <DaemonMessage>[];
+      outputStreamController.stream.listen((event) {
+        outputMessages.add(event);
+      });
+    });
+
+    test('can be instantiated', () async {
+      expect(DaemonServer(), isNotNull);
+    });
+
+    test('version returns daemonVersion', () {
+      expect(daemonServer.version, equals(daemonVersion));
+    });
+
+    test('domainNames returns the correct domains', () {
+      expect(daemonServer.domainNames, ['daemon']);
+    });
+
+    test('kill exits with given exit code', () async {
+      when(() => connection.dispose()).thenAnswer((_) async {});
+      await daemonServer.kill(ExitCode.unavailable);
+      await expectLater(
+        daemonServer.exitCode,
+        completion(ExitCode.unavailable),
+      );
+      verify(() => connection.dispose()).called(1);
+    });
+
+    test('sendEvent sends event to connection', () async {
+      const event = DaemonEvent(
+        domain: 'test',
+        event: 'test',
+        params: {
+          'param': 2,
+        },
+      );
+
+      daemonServer.sendEvent(event);
+
+      await Future<void>.delayed(Duration.zero);
+
+      expect(outputMessages, contains(event));
+    });
+
+    test('routes requests to correct domain', () async {
+      final testDomain = _TestDomain();
+      daemonServer.addDomain(testDomain);
+
+      inputStreamController.add(
+        const DaemonRequest(
+          id: '0',
+          method: 'something',
+          params: {
+            'boo': 'far',
+          },
+          domain: 'test',
+        ),
+      );
+
+      await Future<void>.delayed(Duration.zero);
+
+      expect(outputMessages, [
+        const DaemonResponse.success(
+          id: '0',
+          result: {
+            'foo': 'bar',
+            'boo': 'far',
+          },
+        ),
+      ]);
+    });
+
+    test('handles unknown domains', () async {
+      inputStreamController.add(
+        const DaemonRequest(
+          id: '0',
+          method: 'something',
+          params: {
+            'boo': 'far',
+          },
+          domain: 'test',
+        ),
+      );
+
+      await Future<void>.delayed(Duration.zero);
+
+      expect(outputMessages, [
+        const DaemonResponse.error(
+          id: '0',
+          error: {
+            'message': 'Invalid domain: test',
+          },
+        ),
+      ]);
+    });
+  });
+}
