8ee1a6bf498eecec67082356f9a1e9ff04411aad
Alejandro Santiago <dev@alestiago.com>
2023-07-14 15:27:12 +0100

    test: created create.tests.ts

---
+++ extensions/vscode/src/test/suite/commands/create.test.ts
@@ -0,0 +1,9 @@
+const sinon = require("sinon");
+var proxyquire = require("proxyquire");
+
+import { afterEach, beforeEach } from "mocha";
+import * as assert from "assert";
+
+suite("create command", () => {
+  // TODO(alestiago): Add tests for create command.
+});
