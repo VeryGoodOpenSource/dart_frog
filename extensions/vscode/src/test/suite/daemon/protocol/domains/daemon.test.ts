ed49423178c5b711d5f5015e6ca0ab9e84a399c0
Alejandro Santiago <dev@alestiago.com>
2023-08-14 17:41:25 +0100

    test: tested constructors

--- extensions/vscode/src/test/suite/daemon/protocol/domains/daemon.test.ts
+++ extensions/vscode/src/test/suite/daemon/protocol/domains/daemon.test.ts
@@ -1,10 +1,22 @@
 import * as assert from "assert";
 import {
   DaemonMessage,
+  KillDaemonRequest,
+  RequestVersionDaemonRequest,
   isKillDaemonRequest,
   isReadyDaemonEvent,
   isRequestVersionDaemonRequest,
 } from "../../../../../daemon";
+
+suite("RequestVersionDaemonRequest", () => {
+  test("is a valid requestVersion request", () => {
+    const request = new RequestVersionDaemonRequest("1");
+
+    assert.equal(request.id, "1");
+    assert.equal(request.method, "daemon.requestVersion");
+    assert.equal(request.params, undefined);
+  });
+});
 
 suite("isRequestVersionDaemonRequest", () => {
   test("returns true when it is a RequestVersionDaemonRequest", () => {
@@ -14,6 +26,25 @@ suite("isRequestVersionDaemonRequest", () => {
     );
     const requestObject = DaemonMessage.decode(request)[0];
     assert.equal(isRequestVersionDaemonRequest(requestObject), true);
+  });
+
+  test("returns false when it is not a RequestVersionDaemonRequest", () => {
+    const request = Buffer.from(
+      `[{"method": "daemon.notARequestVersion", "id": "1"}]\n`,
+      "utf8"
+    );
+    const requestObject = DaemonMessage.decode(request)[0];
+    assert.equal(isRequestVersionDaemonRequest(requestObject), false);
+  });
+});
+
+suite("KillDaemonRequest", () => {
+  test("is a valid kill request", () => {
+    const request = new KillDaemonRequest("1");
+
+    assert.equal(request.id, "1");
+    assert.equal(request.method, "daemon.kill");
+    assert.equal(request.params, undefined);
   });
 });
 
@@ -26,6 +57,15 @@ suite("isKillDaemonRequest", () => {
     const requestObject = DaemonMessage.decode(request)[0];
     assert.equal(isKillDaemonRequest(requestObject), true);
   });
+
+  test("returns false when it is not a KillDaemonRequest", () => {
+    const request = Buffer.from(
+      `[{"method": "daemon.notAKill", "id": "1"}]\n`,
+      "utf8"
+    );
+    const requestObject = DaemonMessage.decode(request)[0];
+    assert.equal(isKillDaemonRequest(requestObject), false);
+  });
 });
 
 suite("isReadyDaemonEvent", () => {
@@ -36,5 +76,14 @@ suite("isReadyDaemonEvent", () => {
     );
     const eventObject = DaemonMessage.decode(event)[0];
     assert.equal(isReadyDaemonEvent(eventObject), true);
+  });
+
+  test("returns false when it is not a KillDaemonRequest", () => {
+    const event = Buffer.from(
+      `[{"event":"daemon.notAReady","params":{"version":"0.0.1","processId":8623}}]\n`,
+      "utf8"
+    );
+    const eventObject = DaemonMessage.decode(event)[0];
+    assert.equal(isReadyDaemonEvent(eventObject), false);
   });
 });
