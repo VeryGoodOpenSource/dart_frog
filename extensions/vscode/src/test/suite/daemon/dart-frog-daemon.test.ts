73c46480005a40f5f761b4e6a53070e1415badbf
Alejandro Santiago <dev@alestiago.com>
2023-08-15 14:12:45 +0100

    test: included ready test

--- extensions/vscode/src/test/suite/daemon/dart-frog-daemon.test.ts
+++ extensions/vscode/src/test/suite/daemon/dart-frog-daemon.test.ts
@@ -1,11 +1,63 @@
+const sinon = require("sinon");
+var proxyquire = require("proxyquire");
+import { EventEmitter } from "events";
+
+import { afterEach, beforeEach } from "mocha";
 import assert = require("assert");
-import { DartFrogDaemon } from "../../../daemon";
 
 suite("DartFrogDaemon", () => {
-  test("instance retrieves a singleton", () => {
-    const dartFrogDaemon = DartFrogDaemon.instance;
-    const dartFrogDaemon2 = DartFrogDaemon.instance;
+  let childProcessStub: any;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  let DartFrogDaemon: any;
 
-    assert.equal(dartFrogDaemon, dartFrogDaemon2);
+  beforeEach(() => {
+    childProcessStub = {
+      spawn: sinon.stub(),
+    };
+
+    DartFrogDaemon = proxyquire("../../../daemon/dart-frog-daemon", {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      child_process: childProcessStub,
+    }).DartFrogDaemon;
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  test("instance retrieves a singleton", () => {
+    const daemon = DartFrogDaemon.instance;
+    const daemon2 = DartFrogDaemon.instance;
+
+    assert.equal(daemon, daemon2);
+  });
+
+  suite("isReady", () => {
+    const workingDirectory = "workingDirectory";
+    const readyMessage = `[{"event":"daemon.ready","params":{"version":"0.0.1","processId":94799}}]`;
+
+    test("initially false", () => {
+      const daemon = new DartFrogDaemon();
+      assert.equal(daemon.isReady, false);
+    });
+
+    test("true after beeing invoked", async () => {
+      const daemonProcess = sinon.stub();
+      const daemonStdoutEventEmitter = new EventEmitter();
+      daemonProcess.stdout = daemonStdoutEventEmitter;
+      childProcessStub.spawn
+        .withArgs("dart_frog", ["daemon"], {
+          cwd: workingDirectory,
+        })
+        .returns(daemonProcess);
+
+      const daemon = new DartFrogDaemon();
+
+      const invokePromise = daemon.invoke(workingDirectory);
+      daemonStdoutEventEmitter.emit("data", readyMessage);
+      await invokePromise;
+
+      assert.equal(daemon.isReady, true);
+    });
   });
 });
