fa91d2bcaa90b04397bc3f890608ca7f0c328d53
Erick <erickzanardoo@gmail.com>
2023-08-18 13:47:38 -0300

    feat: change production brick to support external dependencies in the Dockerfile (#926)

---
+++ bricks/dart_frog_prod_server/hooks/src/get_pubspec_lock.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:pubspec_lock/pubspec_lock.dart';
+
+Future<PubspecLock> getPubspecLock(
+  String workingDirectory, {
+  path.Context? pathContext,
+}) async {
+  final pathResolver = pathContext ?? path.context;
+  final pubspecLockFile = File(
+    workingDirectory.isEmpty
+        ? 'pubspec.lock'
+        : pathResolver.join(workingDirectory, 'pubspec.lock'),
+  );
+
+  final content = await pubspecLockFile.readAsString();
+  return content.loadPubspecLockFromYaml();
+}
