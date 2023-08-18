dae930e49e4c89c45e9fb8f6660194af76927cf5
Alejandro Santiago <dev@alestiago.com>
2023-08-18 11:36:00 +0100

    test: setTimeout back to 60000

--- extensions/vscode/src/test/suite/index.ts
+++ extensions/vscode/src/test/suite/index.ts
@@ -13,7 +13,7 @@ export function run(): Promise<void> {
   const mocha = new Mocha({
     ui: "tdd",
     color: true,
-    timeout: 100000,
+    timeout: 60000,
   });
 
   const testsRoot = path.resolve(__dirname, "..");
