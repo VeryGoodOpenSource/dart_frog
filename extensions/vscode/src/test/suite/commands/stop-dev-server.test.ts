d1138c1dec9b188ca174a5f9ca6d197b551fd745
Alejandro Santiago <dev@alestiago.com>
2023-08-23 14:49:03 +0100

    test: added test names

--- extensions/vscode/src/test/suite/commands/stop-dev-server.test.ts
+++ extensions/vscode/src/test/suite/commands/stop-dev-server.test.ts
@@ -81,4 +81,50 @@ suite("stop-dev-server command", () => {
       sinon.assert.notCalled(utilsStub.suggestInstallingDartFrogCLI);
     });
   });
+
+  suite("no running servers information", () => {
+    suite("is shown", () => {
+      test("when daemon is not ready", async () => {});
+
+      test("when no servers are running", async () => {});
+    });
+
+    suite("is not shown", () => {
+      test("when daemon is ready", async () => {});
+
+      test("when a server is running", async () => {});
+    });
+
+    test("start server option runs start server command", async () => {});
+
+    suite("never stops the server", () => {
+      test("when `Start server` option is selected", async () => {});
+
+      test("when `Cancel` option is selected", async () => {});
+
+      test("when dismissied", async () => {});
+    });
+  });
+
+  suite("application quick pick", () => {
+    test("is not shown when there is a single running application", async () => {});
+
+    test("is shown when there is more than a single running application", async () => {});
+
+    test("never stops the server when dismissed", async () => {});
+
+    test("shows appropiate items for each running applications", async () => {});
+
+    test("is disposed after selection", async () => {});
+  });
+
+  suite("progress", () => {
+    test("is shown when stopping server", async () => {});
+
+    test("shows error message when error occurs", async () => {});
+
+    test("stops when error occurs", async () => {});
+  });
+
+  test("stops server", async () => {});
 });
