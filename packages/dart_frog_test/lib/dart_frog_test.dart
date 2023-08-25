56e47bb8bbaf00ae0d8e87e1f7a40145c90a8a28
Erick <erickzanardoo@gmail.com>
2023-08-25 09:49:10 -0300

    Update packages/dart_frog_test/lib/dart_frog_test.dart
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

--- packages/dart_frog_test/lib/dart_frog_test.dart
+++ packages/dart_frog_test/lib/dart_frog_test.dart
@@ -1,4 +1,4 @@
-/// A testing library which makes it easy to test Dart frog services
+/// A testing library which makes it easy to test Dart frog services.
 library dart_frog_test;
 
 export 'src/dart_frog_test.dart';
