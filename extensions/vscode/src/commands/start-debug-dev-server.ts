62a5468aec9c32d9b2be59d9daf9d10751a2517b
Alejandro Santiago <dev@alestiago.com>
2023-08-29 15:42:19 +0100

    docs: enhanced docs

--- extensions/vscode/src/commands/start-debug-dev-server.ts
+++ extensions/vscode/src/commands/start-debug-dev-server.ts
@@ -4,8 +4,8 @@ import { DebugDevServerOptions, debugDevServer, startDevServer } from ".";
  * Starts a Dart Frog application and immediately attaches a debugger to it.
  *
  * @returns A promise that resolves when the server has been started and a debug
- * session has been created. The debug session will not be started if the
- * server fails to start.
+ * session has been created. It might resolve promptly if the server fails to
+ * start.
  */
 export const startDebugDevServer = async (): Promise<void> => {
   const application = await startDevServer();
