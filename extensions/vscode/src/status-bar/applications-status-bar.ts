e1d4a4948b909620480ae1f62f29309e30626b8c
Alejandro Santiago <dev@alestiago.com>
2023-08-30 12:18:37 +0100

    feat: updates status bar

--- extensions/vscode/src/status-bar/applications-status-bar.ts
+++ extensions/vscode/src/status-bar/applications-status-bar.ts
@@ -1,22 +1,70 @@
-import { Disposable, StatusBarAlignment, StatusBarItem, window } from "vscode";
+import {
+  DartFrogApplicationRegistryEventEmitterTypes,
+  DartFrogDaemon,
+} from "../daemon";
+import {
+  Disposable,
+  StatusBarAlignment,
+  StatusBarItem,
+  ThemeColor,
+  window,
+} from "vscode";
 
 export class ApplicationStatusBar implements Disposable {
-  public readonly statusBarItem: StatusBarItem;
+  public readonly startStopStatusBarItem: StatusBarItem;
+  public readonly applicationStatusBarItem: StatusBarItem;
 
   constructor() {
-    this.statusBarItem = window.createStatusBarItem(
+    this.startStopStatusBarItem = window.createStatusBarItem(
       StatusBarAlignment.Left,
-      100
+      10
+    );
+    this.applicationStatusBarItem = window.createStatusBarItem(
+      StatusBarAlignment.Right,
+      10
     );
 
-    this.statusBarItem.text = "$(play) Start Server";
-    // this.statusBarItem.backgroundColor = new ThemeColor(
-    //   "statusBarItem.errorBackground"
-    // );
-    this.statusBarItem.show();
+    this.update();
+    this.startStopStatusBarItem.show();
+
+    const daemon = DartFrogDaemon.instance;
+    daemon.applicationRegistry.on(
+      DartFrogApplicationRegistryEventEmitterTypes.add,
+      this.update.bind(this)
+    );
+    daemon.applicationRegistry.on(
+      DartFrogApplicationRegistryEventEmitterTypes.remove,
+      this.update.bind(this)
+    );
+  }
+
+  private update(): void {
+    const daemon = DartFrogDaemon.instance;
+    const applications = daemon.applicationRegistry.all();
+
+    if (applications.length === 0) {
+      this.startStopStatusBarItem.text = "$(dart-frog) Start Server";
+      this.startStopStatusBarItem.tooltip = "Start development server";
+      this.startStopStatusBarItem.command = "dart-frog.start-dev-server";
+      this.startStopStatusBarItem.backgroundColor = undefined;
+
+      this.applicationStatusBarItem.hide();
+    } else {
+      this.startStopStatusBarItem.text = "$(dart-frog) Stop Server";
+      this.startStopStatusBarItem.tooltip = "Stop development server";
+      this.startStopStatusBarItem.command = "dart-frog.stop-dev-server";
+      this.startStopStatusBarItem.backgroundColor = new ThemeColor(
+        "statusBarItem.warningBackground"
+      );
+
+      const application = applications[0];
+      this.applicationStatusBarItem.text = `$(globe) localhost:${application.port}`;
+      this.applicationStatusBarItem.tooltip = `localhost:${application.port}`;
+      this.applicationStatusBarItem.show();
+    }
   }
 
   dispose() {
-    this.statusBarItem.dispose();
+    this.startStopStatusBarItem.dispose();
   }
 }
