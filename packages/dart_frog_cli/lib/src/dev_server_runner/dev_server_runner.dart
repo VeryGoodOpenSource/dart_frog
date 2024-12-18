eb9cb017d7248cf49a0e72737738a8ac0bb6b3fe
Alejandro Santiago <dev@alestiago.com>
2024-12-18 10:00:01 +0000

    chore: dart format .

--- packages/dart_frog_cli/lib/src/dev_server_runner/dev_server_runner.dart
+++ packages/dart_frog_cli/lib/src/dev_server_runner/dev_server_runner.dart
@@ -234,11 +234,11 @@ class DevServerRunner {
   /// This method will throw a [DartFrogDevServerException] if called after
   /// [stop] has been called.
   ///
-  /// The [arguments] are optional and usually expected to be those from 
-  /// `ArgResults.rest`; since a `--` signals the **end of options** and 
+  /// The [arguments] are optional and usually expected to be those from
+  /// `ArgResults.rest`; since a `--` signals the **end of options** and
   /// disables further option processing from `dart_frog dev`. Any [arguments]
   /// after the `--` are passed into the [Dart tool](https://dart.dev/tools/dart-tool)
-  /// process. 
+  /// process.
   ///
   /// For example, you could use the `--` to enable [experimental flags](https://dart.dev/tools/experiment-flags)
   /// such as macros by doing `dart_frog dev -- --enable-experiment=macros`.
